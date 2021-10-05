41d0d05d77c6946b56252bac85317d50f5488dcc
mark <markpackham1@gmail.com>
2021-10-05 17:18:55 +0100

    adding models
    https://youtu.be/PtQiiknWUcI?t=5147

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from .models import Room
+
+# rooms = [
+#     {'id':1, 'name': 'learn python!'},
+#     {'id':2, 'name': 'learn C++!'},
+#     {'id':3, 'name': 'learn PHP (the best language)!'},
+# ]
 
 def home(request):
-    return render(request, 'home.html')
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room':room}
+    return render(request, 'base/room.html',context)
