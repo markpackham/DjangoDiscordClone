58b1ddd3e04c711f9f37a1ef314bc75ca3067d48
mark <markpackham1@gmail.com>
2021-10-06 16:14:25 +0100

    add update functionality for rooms
    https://youtu.be/PtQiiknWUcI?t=7173

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -20,9 +20,22 @@ def room(request, pk):
 
 def createRoom(request):
     form = RoomForm()
-    
+
     if request.method == 'POST':
         form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form':form}
+    return render(request, 'base/room_form.html', context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
         if form.is_valid():
             form.save()
             return redirect('home')
