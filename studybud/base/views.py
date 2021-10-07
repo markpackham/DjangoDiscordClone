1df71998fa8ec3eab9897a848d0d8f776ec0783e
mark <markpackham1@gmail.com>
2021-10-07 13:41:26 +0100

    restrict user functionality
    https://youtu.be/PtQiiknWUcI?t=10017

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 # Q lets us run more complex queries with Ands and Ors
 from django.db.models import Q
 from django.contrib.auth.models import User
@@ -51,6 +53,7 @@ def room(request, pk):
     context = {'room':room}
     return render(request, 'base/room.html',context)
 
+@login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
 
@@ -63,9 +66,13 @@ def createRoom(request):
     context = {'form':form}
     return render(request, 'base/room_form.html', context)
 
+@login_required(login_url='login')
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+
+    if request.user != room.host:
+        return HttpResponse('You are not allowed here') 
 
     if request.method == 'POST':
         form = RoomForm(request.POST, instance=room)
@@ -76,8 +83,10 @@ def updateRoom(request, pk):
     context = {'form':form}
     return render(request, 'base/room_form.html', context)
 
+@login_required(login_url='login')
 def deleteRoom(request,pk):
     room = Room.objects.get(id=pk)
+
     if request.method == 'POST':
         room.delete()
         return redirect('home')
