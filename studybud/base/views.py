1348cc7c654bb067b6f9ed107edc8990a9a3b0a4
mark <markpackham1@gmail.com>
2021-10-13 16:16:56 +0100

    limit topics amount for homepage
    https://youtu.be/PtQiiknWUcI?t=20871

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -66,7 +66,7 @@ def home(request):
     | Q(description__icontains= q)
     )
 
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
     room_messages = Message.objects.filter(Q(room__name__icontains=q))
 
@@ -190,8 +190,10 @@ def deleteMessage(request, pk):
     return render(request, 'base/delete.html', {'obj': message})
 
 
-def topicsPage():
-    return
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request, 'base/topics.html', {'topics': topics})
 
 def activityPage():
     return
