f5ccb4f048292bca8c0c444461f10a143a7c10d1
mark <markpackham1@gmail.com>
2021-10-14 16:18:42 +0100

    amend user model fields
    https://youtu.be/PtQiiknWUcI?t=25007

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -5,15 +5,14 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-    # name = models.CharField(max_length=200, null=True)
-    # email = models.EmailField(unique=True, null=True)
-    # bio = models.TextField(null=True)
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(unique=True, null=True)
+    bio = models.TextField(null=True)
 
-    # avatar = models.ImageField(null=True, default="avatar.svg")
+    avatar = models.ImageField(null=True, default="avatar.svg")
 
-    # USERNAME_FIELD = 'email'
-    # REQUIRED_FIELDS = []
-    pass
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
 
 class Topic(models.Model):
